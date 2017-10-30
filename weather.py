477198a87d68084f11a8d08fbf4ac2b59e2a81d0
matthex <matthex.koch@googlemail.com>
2017-10-30 21:04:49 +0100

    Weather!

--- weather.py
+++ weather.py
@@ -11,6 +11,6 @@ def get_weather():
     api_key = config["weather-map-api-key"]
     url = 'https://api.openweathermap.org/data/2.5/weather?id=2894003&lang=de&units=metric&APPID=' + api_key #weather for Kaiserslautern
     response = requests.request("GET", url).json()
-    return "In Kaiserslautern ist es " + func(response['weather'][0]['description']) + " bei " + "{:.1f}".format(response['main']['temp']).replace('.', ',') + " Grad Celcius."
+    return "Das Wetter in Kaiserslautern: " + func(response['weather'][0]['description']) + " bei " + "{:.1f}".format(response['main']['temp']).replace('.', ',') + " Grad Celcius."
 
 func = lambda s: s[:1].lower() + s[1:] if s else ''
